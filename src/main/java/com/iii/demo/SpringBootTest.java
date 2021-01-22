f4aa99217939a9977c78611a09f071236f685ee1
RyanWU1003 <ex9405210@gmail.com>
2021-01-22 14:26:35 +0800

    1/22 Secuity 基礎新增

--- src/main/java/com/iii/demo/SpringBootTest.java
+++ src/main/java/com/iii/demo/SpringBootTest.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-//@Controller
+
 @RestController		//包含了@Controller跟@ResponseBody
 public class SpringBootTest {
 	//@ResponseBody
